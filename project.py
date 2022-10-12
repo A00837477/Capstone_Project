8ad528c37fab18b25ba2aa39bb609e2d8b6f1aaf
Javier Cruz Villarreal <A00837477@tec.mx>
2022-10-12 09:47:14 -0500

    Made the Matrix

---
+++ project.py
@@ -0,0 +1,16 @@
+# My name is Javier Cruz Villarreal, a 1st semester University student at ITESM
+# This is my final programming project for my class, where i will be making a special game based on a 6x6 board
+
+
+#Function that makes the matrix
+mat=[["   " , " 0 " , " 1 " , " 2 " , " 3 " , " 4 " , " 5 "],
+     [" 0 " , " * " , " _ " , " X " , " X " , " _ " , " * "],
+     [" 1 " , " _ " , " _ " , " _ " , " _ " , " _ " , " _ "],
+     [" 2 " , " _ " , " _ " , " = " , " = " , " _ " , " _ "],
+     [" 3 " , " _ " , " _ " , " _ " , " _ " , " _ " , " _ "],
+     [" 4 " , " _ " , " _ " , " _ " , " _ " , " _ " , " _ "],
+     [" 5 " , " * " , " _ " , " O " , " O " , " _ " , " * "]]
+#while that keeps the game running until someone wins or loses
+
+#main function
+print(mat)
